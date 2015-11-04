4accd5514016b456d4043a7c0521e0482566fcd1
Eamon <Eamoncorr@gmail.com>
2015-11-04 11:22:32 +1030

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -13,7 +13,7 @@ library(dplyr)
 data <- read.table('./data/household_power_consumption.txt',sep=';', header=TRUE, stringsAsFactors=FALSE)
 
 subset <- filter(data, Date =='1/2/2007'| Date =='2/2/2007') #filter for correct dates
-subSet[,3] <- as.numeric(subSet[,3]) # change to numeric vriable
+subSet[,3] <- as.numeric(subSet[,3]) # change to numeric variable
 dateTime <- paste(subSet[,1], subSet[,2], sep=' ') #paste Date & Time
 subSet$DateTime <- strptime(dateTime, '%d/%m/%Y %H:%M:%S')  # change format & add column to subSet
 with(subSet, plot(DateTime, Global_active_power, type='l', ylab='Global Active Power (Kilowatts)', xlab=''))
